75447298f1d956557e99050054fabd26ddb8b013
gabriel <gabriel.oprescu@diconium.com>
2024-10-14 14:58:24 +0300

    new class added to app

--- app.py
+++ app.py
@@ -1,13 +1,16 @@
 import io
 import json
+import os
 from pathlib import Path
 from contextlib import redirect_stdout
 import pandas as pd
 import plotly.io as pio
+import numpy as np
+import plotly.express as px
+import plotly.graph_objects as go
 import streamlit as st
-import processor
-from processor import process_question
-from agents import save_plot_agent, check_api_key_agent
+from reason_agents import DfCodeAgent, DfOaCodeAgent
+from functions import check_api_key
 
 # otherwise black and white plots are displayed
 pio.templates.default = "plotly"
@@ -19,17 +22,23 @@ if "api_key" not in st.session_state:
 if "valid" not in st.session_state:
     st.session_state.valid = False
 
+if "key_resp" not in st.session_state:
+    st.session_state.key_resp = ""
+
+if "df" not in st.session_state:
+    st.session_state.df = None
+
 
 def submit():
     # api key session value takes the widget value
     st.session_state.api_key = st.session_state.widget
-    st.session_state.widget = ""  # aftter that, delete the value from input widget
+    # st.session_state.widget = ""  # aftter that, delete the value from input widget
 
 
 def refresh_df():
     # refresh df if the value in upload_file changes
-    st.session_state.dff = pd.read_csv(st.session_state.uploaded_file)
-    st.session_state.columns = st.session_state.dff.columns.to_list()  # refresh columns
+    st.session_state.df = pd.read_csv(st.session_state.uploaded_file)
+    st.session_state.columns = st.session_state.df.columns.to_list()  # refresh columns
 
 
 st.title("Analytics chat")
@@ -41,9 +50,13 @@ with st.sidebar:
         "This software allows you to ask questions based on a dataframe you upload."
     )
 
+    select_llm = st.selectbox(
+        "How would you like to be contacted?", ("openai", "gemini")
+    )
+
     # input for api key
     st.text_input(
-        "Gemini API key",
+        "API key",
         type="password",
         placeholder="API KEY",
         key="widget",
@@ -52,17 +65,19 @@ with st.sidebar:
 
     if st.session_state.api_key:
         # test if a call to the model is successful using the api key
-        resp = check_api_key_agent(api_key=st.session_state.api_key)
+        st.session_state.key_resp = check_api_key(
+            api_key=st.session_state.api_key, which=select_llm
+        )
 
-        if resp == "invalid_key":
+        if st.session_state.key_resp == "invalid_key":
             st.markdown(""":red[The key is invalid. Try again]""")
-        elif resp == "valid_key":
+        elif st.session_state.key_resp == "valid_key":
             st.markdown(""":green[The key is valid]""")
             st.session_state.valid = True
-        elif resp == "empty_key":
+        elif st.session_state.key_resp == "empty_key":
             st.markdown(""":orange[The key is empty]""")
         else:
-            st.markdown(f""":orange[{resp}]""")
+            st.markdown(f""":orange[{st.session_state.key_resp}]""")
 
     # upload csv file
     st.file_uploader(
@@ -73,6 +88,17 @@ with st.sidebar:
         on_change=refresh_df,
     )
 
+if st.session_state.key_resp == "valid_key":
+    if st.session_state.df is not None:
+        if select_llm == "openai":
+            cda = DfOaCodeAgent(
+                st.session_state.df, api_key=st.session_state.widget, save_plot=True
+            )
+        if select_llm == "gemini":
+            cda = DfCodeAgent(
+                st.session_state.df, api_key=st.session_state.widget, save_plot=True
+            )
+
 
 # initialize the history of the chat. all messages go here
 if "messages" not in st.session_state:
@@ -82,9 +108,14 @@ if "messages" not in st.session_state:
 for message in st.session_state.messages:
     with st.chat_message(message["role"]):
         if message["type"] == "text":
-            st.text(message["content"])
+            if message.get("content"):
+                st.text(message["content"])
+            if message.get("code_output"):
+                st.text(message["code_output"])
             if message.get("code"):
                 st.code(message.get("code"))
+            if message.get("explanation"):
+                st.markdown(message.get("explanation"))
         else:
             st.image(message["content"])  # display image
             if message.get("code"):
@@ -105,65 +136,81 @@ if prompt := st.chat_input(
 
     # assistent response
     with st.chat_message("assistant"):
+        # breakpoint()
         # take question from user
         question = prompt
 
-        # initialize dff and columns variables
-        if "dff" in st.session_state and "columns" in st.session_state:
-            dff = st.session_state.dff
-            columns = st.session_state.columns
-
+        plot_start = len(os.listdir("./plots"))
         try:
             # get the response from LLM
-            indent_response = process_question(question, columns)
+            resp = cda.generate_content(question)
 
-            # check is the code genetates a plot. if so, the plot is saved
-            check_plot_response = json.loads(
-                save_plot_agent(indent_response).text, strict=False
-            )
+            plot_stop = len(os.listdir("./plots"))
 
-            print("CODE CHECK FOR PLOT:\n" + check_plot_response["answer"])
+            if plot_stop > plot_start:
 
-            # if the code generates plot
-            if check_plot_response["is_plot"] == "true":
-                exec(check_plot_response["answer"])
-                # take the last path from the folder and update history
+                plot_start = plot_stop
+
                 path = sorted(Path("plots").glob("*.png"))[-1]
+
                 st.session_state.messages.append(
                     {
                         "role": "assistant",
                         "content": str(path),
                         "type": "plot",
-                        "code": check_plot_response["answer"],
+                        "code": resp["model"]["answer"],
                     }
                 )
                 # show the code and plot now
+
                 st.image(str(path))
-                st.code(check_plot_response["answer"])
+
+                st.code(resp["answer"])
 
             else:
-                # if it is not plot, capture print statement
-                output_capture = io.StringIO()
 
-                with redirect_stdout(output_capture):
-                    exec(check_plot_response["answer"])
+                if resp["model"]["answer"] == "no code":
+                    # print(resp["model"]["explanation"])
+                    st.session_state.messages.append(
+                        {
+                            "role": "assistant",
+                            "type": "text",
+                            "content": resp["model"]["explanation"],
+                        }
+                    )
+                    st.text(resp["model"]["explanation"])
+                    # st.code(resp["answer"])
+                else:
 
-                captured_output = output_capture.getvalue()
+                    if resp.get("code_run"):
 
-                st.session_state.messages.append(
-                    {
-                        "role": "assistant",
-                        "content": captured_output,
-                        "type": "text",
-                        "code": check_plot_response["answer"],
-                    }
-                )
-                st.text(captured_output)
-                st.code(check_plot_response["answer"])
-
-            # refresh session variable if dff of columns were changed
-            st.session_state.dff = dff
-            st.session_state.columns = dff.columns.to_list()
+                        if resp["code_run"].get("output") is not None:
+                            st.session_state.messages.append(
+                                {
+                                    "role": "assistant",
+                                    "type": "text",
+                                    "code_output": resp["code_run"]["output"],
+                                    "code": resp["model"]["answer"],
+                                    "explanation": resp["model"]["explanation"],
+                                }
+                            )
+                            st.text(resp["code_run"]["output"])
+                            st.code(resp["model"]["answer"])
+                            st.markdown(resp["model"]["explanation"])
+                        else:
+                            # print(resp["code_run"]["exception"])
+                            st.session_state.messages.append(
+                                {
+                                    "role": "assistant",
+                                    "type": "text",
+                                    "code_output": resp["code_run"]["output"],
+                                    "code": "",
+                                    "explanation": resp["model"]["explanation"],
+                                }
+                            )
+                            st.text(resp["code_run"]["exception"])
+                            st.code(resp["model"]["answer"])
+                            st.markdown(resp["model"]["explanation"])
 
         except Exception as e:
             print(e)
